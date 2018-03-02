84bbf3910b2b7164684daacd0b55cf3cd24ebfd7
Crypto KAERU <36767880+CryptoKAERU@users.noreply.github.com>
2018-03-02 18:31:24 +0000

    Add files via upload

--- js/new_options.js
+++ js/new_options.js
@@ -12,11 +12,12 @@ function saveConfiguration() {
 
 	var manual_list = {};
 	$("#tbl_manual_list .domain").each(function (index, elem) {
-		var tmp_url = new URL('http://' + $(elem).text());
-		if (tmp_url != null) {
+		try {
+			var tmp_url = new URL('http://' + $(elem).text());
 			manual_list[tmp_url.hostname] = {
 				"url": tmp_url.hostname
 			};
+		} catch (e) {
 		}
 	});
 	dictConfig["manual_list"] = manual_list;
@@ -59,10 +60,10 @@ document.addEventListener('DOMContentLoaded', function () {
 			var manual_list = dictConfig["manual_list"];
 			var tbl_manual_list = $("#tbl_manual_list>tbody");
 			Object.keys(manual_list).forEach(function (key) {
-				tbl_manual_list.append('<tr><td><div class="message-header"><div class="icon_url"><span class="icon"><img src="http://www.google.com/s2/favicons?domain='+manual_list[key].url+'"></span><span class="domain">'+manual_list[key].url+'</span></div><button class="delete"></button></div></td></tr>');
+				tbl_manual_list.append('<tr><td><div class="message-header"><div class="icon_url"><span class="icon"><img src="http://www.google.com/s2/favicons?domain=' + manual_list[key].url + '"></span><span class="domain">' + manual_list[key].url + '</span></div><button class="delete"></button></div></td></tr>');
 			});
 
-			$("#tbl_manual_list .delete").on('click', function(e) {
+			$("#tbl_manual_list .delete").on('click', function (e) {
 				e.preventDefault();
 				$(this).off("click").closest("tr").remove();
 				saveConfiguration();
@@ -75,34 +76,54 @@ document.addEventListener('DOMContentLoaded', function () {
 	document.getElementById('cheBlockPage').addEventListener('change', saveConfiguration);
 	document.getElementById('chePunyAlert').addEventListener('change', saveConfiguration);
 
-	$("#add_manual_url").on('click', function(e) {
+	$("#add_manual_url").on('click', function (e) {
 		e.preventDefault();
 		var input_obj = $("#manual_url");
 		var url_val = input_obj.val();
-		if(url_val == ""){
+		if (url_val === "") {
 			return;
 		}
-		try{
+		try {
 			url_val = new URL(url_val);
-		}catch(e){
-			try{
-				url_val = new URL('http://'+url_val);
-			}catch(ee){
+		} catch (e) {
+			try {
+				url_val = new URL('http://' + url_val);
+			} catch (ee) {
 				input_obj.addClass("is-danger");
 				return;
 			}
 		}
+		if (url_val.hostname === "" || !isUrlValid(url_val.href)) {
+			input_obj.addClass("is-danger");
+			return;
+		}
 		input_obj.removeClass("is-danger");
 		input_obj.val("");
 		var tbl_manual_list = $("#tbl_manual_list>tbody");
-		tbl_manual_list.append('<tr><td><div class="message-header"><div class="icon_url"><span class="icon"><img src="http://www.google.com/s2/favicons?domain='+url_val.hostname+'"></span><span class="domain">'+url_val.hostname+'</span></div><button class="delete"></button></div></td></tr>');
+		tbl_manual_list.append('<tr><td><div class="message-header"><div class="icon_url"><span class="icon"><img src="http://www.google.com/s2/favicons?domain=' + url_val.hostname + '"></span><span class="domain">' + url_val.hostname + '</span></div><button class="delete"></button></div></td></tr>');
 
 		saveConfiguration();
 
-		$("#tbl_manual_list .delete").on('click', function(e) {
+		$("#tbl_manual_list .delete").on('click', function (e) {
 			e.preventDefault();
 			$(this).off("click").closest("tr").remove();
 			saveConfiguration();
 		});
 	});
+
+	$("#manual_url").on('keypress', function (e) {
+		if (e.keyCode && e.keyCode == 13) {
+			e.preventDefault();
+			$("#add_manual_url").triggerHandler("click");
+		}
+	});
 });
+
+function isUrlValid(url) {
+	var res = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+	if (res == null) {
+		return false;
+	} else {
+		return true;
+	}
+}
