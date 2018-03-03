02c4b4e09f5da9cfa429103f57496e41258cdb99
Crypto KAERU <36767880+CryptoKAERU@users.noreply.github.com>
2018-03-03 22:55:57 +0000

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -120,135 +120,136 @@ function doCheckAction(currentUrl, eventType, tabActive, tabId) {
 				}
 			}
 		}
-	});
 
+		if (check_type === "PHISHING") {
 
-	if (check_type === "PHISHING") {
+			if (dictConfig["cheAlert"]) {
+				var textTitle = chrome.i18n.getMessage("alePhishingNotificationTitle");
+				var textBody = chrome.i18n.getMessage("alePhishingNotificationBody");
 
-		if (dictConfig["cheAlert"]) {
-			var textTitle = chrome.i18n.getMessage("alePhishingNotificationTitle");
-			var textBody = chrome.i18n.getMessage("alePhishingNotificationBody");
+				chrome.notifications.create("tabId_" + tabId, {
+						type: "basic",
+						title: textTitle,
+						iconUrl: "img/red_48.png",
+						message: textBody + hostName,
+						contextMessage: chrome.i18n.getMessage("badgeTitle"),
+						requireInteraction: true,
+						isClickable: true
+					}, function (notificationId) {
+					}
+				);
+			}
 
-			chrome.notifications.create("tabId_" + tabId, {
-					type: "basic",
-					title: textTitle,
-					iconUrl: "img/red_48.png",
-					message: textBody + hostName,
-					contextMessage: chrome.i18n.getMessage("badgeTitle"),
-					requireInteraction: true,
-					isClickable: true
-				}, function (notificationId) {
-				}
-			);
+			chrome.browserAction.setIcon({path: "img/red.png"});
+			chrome.browserAction.setBadgeText({"text": "BAD"});
+			chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+			chrome.browserAction.setTitle({title: chrome.i18n.getMessage("alePhishingNotificationTitle")});
+			chrome.browserAction.setPopup({"popup": "ui/popup.html"})
 		}
+		else if (check_type === "SCAM") {
 
-		chrome.browserAction.setIcon({path: "img/red.png"});
-		chrome.browserAction.setBadgeText({"text": "BAD"});
-		chrome.browserAction.setBadgeBackgroundColor({color: "red"});
-		chrome.browserAction.setTitle({title: chrome.i18n.getMessage("alePhishingNotificationTitle")});
-		chrome.browserAction.setPopup({"popup": "ui/popup.html"})
-	}
-	else if (check_type === "SCAM") {
+			if (dictConfig["cheAlert"]) {
+				var textTitle = chrome.i18n.getMessage("aleScamNotificationTitle");
+				var textBody = chrome.i18n.getMessage("aleScamNotificationBody");
 
-		if (dictConfig["cheAlert"]) {
-			var textTitle = chrome.i18n.getMessage("aleScamNotificationTitle");
-			var textBody = chrome.i18n.getMessage("aleScamNotificationBody");
+				chrome.notifications.create("tabId_" + tabId, {
+						type: "basic",
+						title: textTitle,
+						iconUrl: "img/red_48.png",
+						message: textBody + hostName,
+						contextMessage: chrome.i18n.getMessage("badgeTitle"),
+						requireInteraction: true,
+						isClickable: true
+					}, function (notificationId) {
+					}
+				);
+			}
 
-			chrome.notifications.create("tabId_" + tabId, {
-					type: "basic",
-					title: textTitle,
-					iconUrl: "img/red_48.png",
-					message: textBody + hostName,
-					contextMessage: chrome.i18n.getMessage("badgeTitle"),
-					requireInteraction: true,
-					isClickable: true
-				}, function (notificationId) {
-				}
-			);
+			chrome.browserAction.setIcon({path: "img/red.png"});
+			chrome.browserAction.setBadgeText({"text": "BAD"});
+			chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+			chrome.browserAction.setTitle({title: chrome.i18n.getMessage("aleScamNotificationTitle")});
+			chrome.browserAction.setPopup({"popup": "ui/popup.html"})
 		}
+		else if (check_type === "PUNY") {
 
-		chrome.browserAction.setIcon({path: "img/red.png"});
-		chrome.browserAction.setBadgeText({"text": "BAD"});
-		chrome.browserAction.setBadgeBackgroundColor({color: "red"});
-		chrome.browserAction.setTitle({title: chrome.i18n.getMessage("aleScamNotificationTitle")});
-		chrome.browserAction.setPopup({"popup": "ui/popup.html"})
-	}
-	else if (check_type === "PUNY") {
-
-		var do_notificationFlag = false;
-		if (eventType === "onRequest") {
-			if (dictConfig["chePunyAlert"] && (lastTabDomains["tabId_" + tabId] == null || lastTabDomains["tabId_" + tabId] !== domainName)) {
+			var do_notificationFlag = false;
+			if (eventType === "onRequest") {
+				if (dictConfig["chePunyAlert"] && (lastTabDomains["tabId_" + tabId] == null || lastTabDomains["tabId_" + tabId] !== domainName)) {
+					do_notificationFlag = true;
+				}
+			}
+			else if (eventType !== "onRequest" && dictConfig["chePunyAlert"]) {
 				do_notificationFlag = true;
 			}
+
+			if (do_notificationFlag) {
+				var textTitle = chrome.i18n.getMessage("alePunyNotificationTitle");
+				var textBody = chrome.i18n.getMessage("alePunyNotificationBody");
+
+				chrome.notifications.create("tabId_" + tabId, {
+						type: "basic",
+						title: textTitle,
+						iconUrl: "img/red_48.png",
+						message: textBody + domainName,
+						contextMessage: chrome.i18n.getMessage("badgeTitle"),
+						requireInteraction: false,
+						isClickable: true
+					}, function (notificationId) {
+					}
+				);
+			}
+
+			chrome.browserAction.setIcon({path: "img/red.png"});
+			chrome.browserAction.setBadgeText({"text": "Puny"});
+			chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+			chrome.browserAction.setTitle({title: chrome.i18n.getMessage("alePunyNotificationTitle")});
+			chrome.browserAction.setPopup({"popup": "ui/popup.html"})
 		}
-		else if (eventType !== "onRequest" && dictConfig["chePunyAlert"]) {
-			do_notificationFlag = true;
+		else if (check_type === "WHITE") {
+
+			var do_notificationFlag = false;
+			if (dictConfig["cheWhiteAlert"] && (lastTabDomains["tabId_" + tabId] == null || lastTabDomains["tabId_" + tabId] !== domainName)) {
+				do_notificationFlag = true;
+			}
+
+			if (do_notificationFlag) {
+				//var textTitle = chrome.i18n.getMessage("aleWhiteNotificationTitle");
+				var textBody = chrome.i18n.getMessage("aleWhiteNotificationBody");
+
+				chrome.notifications.create("tabId_" + tabId, {
+						type: "basic",
+						title: whiteSite.title,
+						iconUrl: "img/green_48.png",
+						message: textBody,
+						contextMessage: chrome.i18n.getMessage("badgeTitle"),
+						requireInteraction: false,
+						isClickable: true
+					}, function (notificationId) {
+					}
+				);
+			}
+
+			chrome.browserAction.setIcon({path: "img/green.png"});
+			chrome.browserAction.setBadgeText({"text": "✔"});
+			chrome.browserAction.setBadgeBackgroundColor({color: "green"});
+			var trustedTitle = '"' + whiteSite.title + '" ' + chrome.i18n.getMessage("aleWhiteNotificationTitle");
+			chrome.browserAction.setTitle({title: trustedTitle});
+			chrome.browserAction.setPopup({"popup": "ui/popup.html"})
+		}
+		else {
+			chrome.browserAction.setTitle({title: chrome.i18n.getMessage("badgeTitle")});
+			chrome.browserAction.setBadgeText({"text": ""});
+			chrome.browserAction.setIcon({path: "img/gray.png"});
+			chrome.browserAction.setPopup({"popup": ""})
 		}
 
-		if (do_notificationFlag) {
-			var textTitle = chrome.i18n.getMessage("alePunyNotificationTitle");
-			var textBody = chrome.i18n.getMessage("alePunyNotificationBody");
-
-			chrome.notifications.create("tabId_" + tabId, {
-					type: "basic",
-					title: textTitle,
-					iconUrl: "img/red_48.png",
-					message: textBody + domainName,
-					contextMessage: chrome.i18n.getMessage("badgeTitle"),
-					requireInteraction: false,
-					isClickable: true
-				}, function (notificationId) {
-				}
-			);
+		if (dictConfig["cheAlert"] || dictConfig["chePunyAlert"] || dictConfig["cheWhiteAlert"]) {
+			lastTabDomains["tabId_" + tabId] = domainName;
 		}
 
-		chrome.browserAction.setIcon({path: "img/red.png"});
-		chrome.browserAction.setBadgeText({"text": "Puny"});
-		chrome.browserAction.setBadgeBackgroundColor({color: "red"});
-		chrome.browserAction.setTitle({title: chrome.i18n.getMessage("alePunyNotificationTitle")});
-		chrome.browserAction.setPopup({"popup": "ui/popup.html"})
-	}
-	else if (check_type === "WHITE") {
+	});
 
-		var do_notificationFlag = false;
-		if (dictConfig["cheWhiteAlert"] && (lastTabDomains["tabId_" + tabId] == null || lastTabDomains["tabId_" + tabId] !== domainName)) {
-			do_notificationFlag = true;
-		}
-
-		if (do_notificationFlag) {
-			//var textTitle = chrome.i18n.getMessage("aleWhiteNotificationTitle");
-			var textBody = chrome.i18n.getMessage("aleWhiteNotificationBody");
-
-			chrome.notifications.create("tabId_" + tabId, {
-					type: "basic",
-					title: whiteSite.title,
-					iconUrl: "img/green_48.png",
-					message: textBody,
-					contextMessage: chrome.i18n.getMessage("badgeTitle"),
-					requireInteraction: false,
-					isClickable: true
-				}, function (notificationId) {
-				}
-			);
-		}
-
-		chrome.browserAction.setIcon({path: "img/green.png"});
-		chrome.browserAction.setBadgeText({"text": "✔"});
-		chrome.browserAction.setBadgeBackgroundColor({color: "green"});
-		var trustedTitle = '"' + whiteSite.title + '" ' + chrome.i18n.getMessage("aleWhiteNotificationTitle");
-		chrome.browserAction.setTitle({title: trustedTitle});
-		chrome.browserAction.setPopup({"popup": "ui/popup.html"})
-	}
-	else {
-		chrome.browserAction.setTitle({title: chrome.i18n.getMessage("badgeTitle")});
-		chrome.browserAction.setBadgeText({"text": ""});
-		chrome.browserAction.setIcon({path: "img/gray.png"});
-		chrome.browserAction.setPopup({"popup": ""})
-	}
-
-	if (dictConfig["cheAlert"] || dictConfig["chePunyAlert"] || dictConfig["cheWhiteAlert"]) {
-		lastTabDomains["tabId_" + tabId] = domainName;
-	}
 }
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
